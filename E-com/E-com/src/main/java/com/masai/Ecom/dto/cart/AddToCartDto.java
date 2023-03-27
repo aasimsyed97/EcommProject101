fa9536a3cfda25db33e0a1103d3f30f67dfabdeb
aasimsyed97 <sayedaasim555@gamail.com>
2023-03-27 23:13:32 +0530

    update

--- E-com/E-com/src/main/java/com/masai/Ecom/dto/cart/AddToCartDto.java
+++ E-com/E-com/src/main/java/com/masai/Ecom/dto/cart/AddToCartDto.java
@@ -21,29 +21,29 @@ public class AddToCartDto {
                 ", quantity=" + quantity +
                 ",";
     }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-
-    public Integer getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
+//
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//
+//    public Integer getProductId() {
+//        return productId;
+//    }
+//
+//    public void setProductId(Integer productId) {
+//        this.productId = productId;
+//    }
+//
+//    public Integer getQuantity() {
+//        return quantity;
+//    }
+//
+//    public void setQuantity(Integer quantity) {
+//        this.quantity = quantity;
+//    }
 }
