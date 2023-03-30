ad65c93ca00107e12eb3e67096708295484a52f9
aasimsyed97 <sayedaasim555@gamail.com>
2023-03-30 12:21:33 +0530

    added sample class

---
+++ E-com/E-com/src/main/java/com/masai/Ecom/Model/Sample.java
@@ -0,0 +1,38 @@
+package com.masai.Ecom.Model;
+
+public class Sample { 
+	 private int id;
+	 private String name;
+	 private String add;
+	public Sample() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Sample(int id, String name, String add) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.add = add;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	 
+	 
+
+}
